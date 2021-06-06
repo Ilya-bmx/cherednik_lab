794dd6158d69d2f2bf78472b279abfa233b6ab1b
4-16 Павел С <sumcov.pol@gmail.com>
2021-06-06 21:44:31 +0300

    comments + deletes

--- src/main/java/com/company/main.py
+++ src/main/java/com/company/main.py
@@ -1,77 +1,34 @@
 from sys import setrecursionlimit
 import SR_simple_field as register
 
-n = []
-table_phi = []
-table_ksi = []
-
-# test comment
-
-def phi_ksi_fun(vec, x, mode):
-    number = 0
-    vec.append(x)
-    for i in range(len(vec) - 1, -1, -1):
-        number += 2 ** (len(vec) - 1 - i) * vec[i]
-    if mode == "phi":
-        return table_phi[number]
-    elif mode == "ksi":
-        return table_ksi[number]
-
-
-def h_fun(vec, x):
-    xn = phi_ksi_fun(vec.copy(), x, "phi")
-    yn = phi_ksi_fun(vec.copy(), x, "ksi")
-    for i in range(len(vec) - 1):
-        vec[i] = vec[i + 1]
-    vec.pop(-1)
-    vec.append(xn)
-    return vec
-
-
-def f_fun(vec, x):
-    return phi_ksi_fun(vec.copy(), phi_ksi_fun(vec.copy(), x, "phi"), "ksi")
-
-
-def dec_to_bin(number):
-    global n
-    res_vec = []
-    for i in range(n):
-        res_vec.append(number % 2)
-        number //= 2
-    res_vec.reverse()
-    return res_vec
-
-
-def bin_to_dec(vec):
-    number = 0
-    for i in range(len(vec) - 1, -1, -1):
-        number += 2 ** (len(vec) - 1 - i) * vec[i]
-    return number
+# Функция phi_psi_fun ищет в таблицах Фи или Пси элемент,
+# который нужно взять из таблицы и добавить в вектор состояния.
+# Возвращает нужный элемент.
+# Для поиска добавляет 0 или 1 в конец вектора состояния, переводит полученный вектор в 10-ое число,
+# затем забирает из таблицы Фи или Пси элемент с таким индексом
 
 
 # ориентированный граф
 def make_reach_table():
-    global n
     reach_table = []
     register_for_zeroes = register.SR(2, register.to_bin(0, register.rn), 'table', 'functions/fi.txt', 'functions/psi.txt')
     register_for_one = register.SR(2, register.to_bin(0, register.rn), 'table', 'functions/fi.txt', 'functions/psi.txt')
-    for i in range(2 ** n):
-        table_line = [0] * 2 ** n
-        print("register_for_zeroes: {}".format(register_for_zeroes.s))
+    for i in range(2 ** register.rn):
+        table_line = [0] * 2 ** register.rn
+        # print("register_for_zeroes: {}".format(register_for_zeroes.s))
         table_line[register_for_zeroes.step(0)] = 1
-        print("register_for_one: {}".format(register_for_one.s))
+        # print("register_for_one: {}".format(register_for_one.s))
         table_line[register_for_one.step(1)] = 1
         reach_table.append(table_line)
     return reach_table
 
 
 def depth_first_search(reach_table, mode):
-    global n
     stack = []
     marked = []
     result = []
 
-    for i in range(2 ** n):
+    for i in range(2 ** register.rn):
         flag = False
         for item in result:
             if i in item:
@@ -139,92 +96,20 @@ def dps_rec(reach_table, i, marked, stack, result, mode):
                 stack.pop(-1)
 
 
-def equal_prep():
-    global n
-    equal_array = []
-    state_info = []
-    for i in range(2 ** n):
-        state = dec_to_bin(i)
-        for j in [0, 1]:
-            state_info.append(bin_to_dec(h_fun(state.copy(), j)))
-            state_info.append(f_fun(state.copy(), j))
-        equal_array.append(state_info)
-        state_info = []
-    return equal_array
-
-
-def equal_search(equal_array):
-    equal_classes = []
-    equal_class = []
-    for i in range(len(equal_array)):
-        flag = False
-        for item in equal_classes:
-            if i in item:
-                flag = True
-                break
-        if flag:
-            continue
-        else:
-            equal_class.append(i)
-            for j in range(i + 1, len(equal_array)):
-                if equal_array[i][1] == equal_array[j][1] and equal_array[i][3] == equal_array[j][3]:
-                    equal_class.append(j)
-            equal_classes.append(equal_class)
-            equal_class = []
-    return equal_classes
-
-
-def find_class(classes, item):
-    for i in range(len(classes)):
-        if item in classes[i]:
-            return i
-
-
-def equal_search_final(equal_array, equal_classes):
-    new_classes = []
-    new_class = []
-    for item in equal_classes:
-        for i in range(len(item)):
-            flag = False
-            for s in new_classes:
-                if item[i] in s:
-                    flag = True
-                    break
-            if flag:
-                continue
-            else:
-                new_class.append(item[i])
-                class_0 = find_class(equal_classes, equal_array[item[i]][0])
-                class_1 = find_class(equal_classes, equal_array[item[i]][2])
-                for j in range(i + 1, len(item)):
-                    if find_class(equal_classes, equal_array[item[j]][0]) == class_0 and find_class(equal_classes,
-                                                                                                    equal_array[item[j]][2]) == class_1:
-                        new_class.append(item[j])
-                new_classes.append(new_class)
-                new_class = []
-    return new_classes
-
-
 def main():
-    with open("params.txt", 'r') as file:
-        lines = file.readlines()
-        global table_phi, table_ksi, n
-        n = int(lines[0])
-        table_phi = list(map(lambda x: int(x), lines[2].replace("\n", "").split(" ")))
-        table_ksi = list(map(lambda x: int(x), lines[4].replace("\n", "").split(" ")))
-    # setrecursionlimit(2**n + 10)
+
     reach_table = make_reach_table()
-    # with open("table.txt", 'w') as table:
-    #     for item in reach_table:
-    #         table.write("{}\n".format(','.join(str(e) for e in item).replace("[", "").replace("]", "")))
-    links = depth_first_search(reach_table, "normal")
-    print("Automat is linked") if len(links) == 1 else print("Automat is not linked")
-    print("Linked components: {}".format(links))
-    print("Total: {} linked components".format(len(links)))
-    strong_links = depth_first_search(reach_table, "strong")
-    print("Automat is strongly linked") if len(strong_links) == 1 else print("Automat is not strongly linked")
-    print("Strongly linked components: {}".format(strong_links))
-    print("Total: {} strongly linked components".format(len(strong_links)))
+
+    links = depth_first_search(reach_table, "normal")           # Ищем компоненты связности
+    print("Автомат связный") if len(links) == 1 \
+        else print("Автомат несвязный, потому что делится на {} компонент связности".format(links))
+    print("Компоненты связности: {}".format(links))
+
+    strong_links = depth_first_search(reach_table, "strong")    # Ищем компоненты сильной связности
+    print("Автомат сильно связный") if len(strong_links) == 1 \
+        else print("Автомат не сильно связный, "
+                   "потому что делится на {} компонент сильной связности".format(len(strong_links)))
+    print("Компоненты сильной связности: {}".format(strong_links))
 
 
 if __name__ == "__main__":
